9327bca585a5e3b32e447dda58e1372d717597b5
Obumuneme Nwabude <obumnwabude@gmail.com>
2022-06-17 18:59:52 +0100

    add navbar and make it responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 const contactForm = document.querySelector('form[contact]');
+const menuIcon = document.querySelector('[menu-icon]');
+const nav = document.querySelector('nav');
 
 contactForm.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -7,3 +9,29 @@ contactForm.addEventListener('submit', (event) => {
   contactForm.reset();
   alert('Submitted, Thanks!');
 });
+
+menuIcon.addEventListener('click', () => nav.classList.toggle('toggled'));
+
+document
+  .querySelectorAll('nav ul a')
+  .forEach((a) =>
+    a.addEventListener('click', () => nav.classList.remove('toggled'))
+  );
+
+const isHeroInView = () => {
+  const hero = document.querySelector('[hero]').getBoundingClientRect();
+  return (
+    hero.bottom > 0 &&
+    hero.right > 0 &&
+    hero.left < (window.innerWidth || document.documentElement.clientWidth) &&
+    hero.top < (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+window.onscroll = () => {
+  if (isHeroInView()) {
+    nav.classList.remove('scrolled');
+  } else {
+    nav.classList.add('scrolled');
+  }
+};
